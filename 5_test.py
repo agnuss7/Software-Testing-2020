bcc30ed28132a02bd3ea2577a722f70b09d9799d
agnuss7 <agne.burokaite@gmail.com>
2020-04-28 01:01:01 +0300

    5 baigtas

--- 5_test.py
+++ 5_test.py
@@ -1,21 +1,28 @@
 from selenium import webdriver
 import pytest
-#register=["agne","burokaite","aggie@mail.com","86977887","gatve"]
-register=["aggie@mail.com","86977887","gatve"]
+import random
+register=[""]
 class Testing():
+	#jei scope butu 'function', kiekvienam testavimui atsidarytu ir uzsidarytu naujas skirtukas
+	#su 'class','session' ir 'module' testavimas ivyksta vienam skirtuke, baigus jis uzsidaro
 	@pytest.fixture(scope="class")
 	def browser(self):
 		bowser=webdriver.Firefox()
 		yield bowser
 		bowser.quit()
+	#kiekviename teste bus skirtingo ilgio masyvas 'register'
 	@pytest.fixture(autouse=True)
 	def auto(self):
-		register.insert(0,"aggie")
-	@pytest.mark.parametrize("link",["http://suninjuly.github.io/registration2.html","http://suninjuly.github.io/registration1.html"])
+		global register
+		register=["a"]
+		for i in range(random.randint(1,6)):
+			register.append("a")
+	#testavimas vyks 6 kartus
+	@pytest.mark.parametrize("link",["http://suninjuly.github.io/registration2.html","http://suninjuly.github.io/registration1.html","http://suninjuly.github.io/registration2.html","http://suninjuly.github.io/registration1.html","http://suninjuly.github.io/registration2.html","http://suninjuly.github.io/registration1.html"])
 	def test(self,browser,link):
 		browser.get(link)
 		inputs=browser.find_elements_by_class_name("form-control")
-		assert len(inputs)==len(register), "reikia {} langeliu".format(len(inputs))
+		assert len(inputs)<=len(register), "reikia maziausiai {} langeliu".format(len(inputs))
 		for index,x in enumerate(inputs):
 			x.send_keys(register[index])
 		butt1=browser.find_element_by_css_selector(".btn-default")
